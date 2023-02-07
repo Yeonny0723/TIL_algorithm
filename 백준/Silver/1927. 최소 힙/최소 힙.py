a183967ae679006512e4e7ae222fb0544b89570f
Juyeon Kim <70524037+Yeonny0723@users.noreply.github.com>
2023-02-07 17:14:19 +0900

    [Silver II] Title: 최소 힙, Time: 276 ms, Memory: 118528 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,15 @@
+import heapq
+import sys
+
+input = sys.stdin.readline
+
+q = []
+for _ in range(int(input())):
+    num = int(input())
+    if num == 0:
+        if q:
+            print(heapq.heappop(q))
+        else:
+            print(0)
+    else: 
+        heapq.heappush(q, num)
