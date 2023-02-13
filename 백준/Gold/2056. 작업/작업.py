e6eea6531380de02a5157f1edeaabae13eee0f8e
Juyeon Kim <70524037+Yeonny0723@users.noreply.github.com>
2023-02-13 19:37:32 +0900

    [Gold IV] Title: 작업, Time: 308 ms, Memory: 134728 KB -BaekjoonHub

--- 백준/Gold/2056. 작업/작업.py
+++ 백준/Gold/2056. 작업/작업.py
@@ -1,21 +1,44 @@
 """
-모든 노드를 순회하며, 누적 비용 구한 후, 그 중 최소값을 출력
+소요되는 시간의 최소값은, 
 선행관계에 있는 작업들 중 가장 늦게 끝나는 작업 + 현재 작업 = 현재 작업이 끝나는 시간
 """
+import sys
+from collections import deque
 
-n = int(input())
-costs = [0 for _ in range(n+1)] # 비용
-graph = [[] for _ in range(n+1)] # 그래프 
-for node in range(1, n+1): # 이전 노드를 배열로 갖는 그래프 생성 
-    cost, m, *lst = map(int, input().split()) # 비용, 이전노드개수
-    costs[node] = cost
-    for preNode in lst:
-        graph[node].append(preNode) 
-        
-for node in range(1, n+1): # 모든 노드 순회
-    newCost = 0 
-    for preNode in graph[node]: # 이전 노드 순회
-        newCost = max(newCost, costs[preNode]) # 이전 노드의 
-    costs[node] += newCost
-print(max(costs)) 
+input = sys.stdin.readline
 
+def topological_sort():
+    
+    q = deque()
+    for i in range(1, n+1):
+        if indegree[i] == 0:
+            q.append(i)
+            dp[i] = costs[i]
+            
+    while q:
+        node = q.popleft()
+        for nxtNode in graph[node]:
+            indegree[nxtNode] -= 1
+            dp[nxtNode] = max(dp[node] + costs[nxtNode], dp[nxtNode])
+            if indegree[nxtNode] == 0:
+                q.append(nxtNode)
+    
+    return max(dp)
+    
+if __name__ == "__main__":
+    n = int(input())
+    indegree = [0] * (n+1)
+    costs = [0] * (n+1)
+    graph = [[] for _ in range(n+1)]
+    dp = [0] * (n+1) 
+    for node in range(1,n+1):
+        lst = list(map(int, input().rstrip().split()))
+        iTime, iIndegree = lst[0], lst[1] # 소요시간, 선행작업수
+        costs[node] = iTime
+        indegree[node] = lst[1] # 이전 개수  
+        if indegree == 0:
+            continue
+        for pre in lst[2:]:
+            graph[pre].append(node)
+    
+    print(topological_sort())
